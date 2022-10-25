d91615c394ff75c620386dd459e0eb24f425b440
Sejal-04 <116653545+Sejal-04@users.noreply.github.com>
2022-10-25 20:18:25 +0530

    vercel test upload

---
+++ src/routes/page1/index.jsx
@@ -0,0 +1,14 @@
+import React, {Fragment, useState} from "react";
+
+const RouteGGNT = () =>{
+
+    const [pokemon, setPokemon] = useState([])
+
+    return(
+        <Fragment>
+            <h1>route 1</h1>
+        </Fragment>
+    );
+}
+
+export default RouteGGNT;
